357fb3ab0da965a9f4cd36a046843dc3d60b0b2d
Khaled Omar <khaledomar4797@gmail.com>
2020-12-16 12:04:42 -0500

    Add project files.

---
+++ FoodSpin.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FoodSpin.WebMVC.Startup))]
+namespace FoodSpin.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
