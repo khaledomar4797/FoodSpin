af946e67ed292fbd5f4121aa60c39f29d69b9af1
Khaled Omar <khaledomar4797@gmail.com>
2020-12-21 11:42:09 -0500

    Reset database tables

--- FoodSpin.Data/Migrations/202012182349190_initial-setup.cs
+++ FoodSpin.Data/Migrations/202012182349190_initial-setup.cs
@@ -3,10 +3,75 @@ namespace FoodSpin.Data.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class InitialCreate : DbMigration
+    public partial class initialsetup : DbMigration
     {
         public override void Up()
         {
+            CreateTable(
+                "dbo.Cart",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        CartId = c.String(),
+                        Count = c.Int(nullable: false),
+                        DateCreated = c.DateTime(nullable: false),
+                        ProductId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID)
+                .ForeignKey("dbo.Product", t => t.ProductId, cascadeDelete: true)
+                .Index(t => t.ProductId);
+            
+            CreateTable(
+                "dbo.Product",
+                c => new
+                    {
+                        ProductId = c.Int(nullable: false, identity: true),
+                        OwnerId = c.Guid(nullable: false),
+                        ProductName = c.String(nullable: false),
+                        ProductDescription = c.String(nullable: false),
+                        ProductPrice = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        ProductCategory = c.Int(nullable: false),
+                        ProductImage = c.String(nullable: false),
+                        ProductQuantity = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ProductId);
+            
+            CreateTable(
+                "dbo.OrderDetail",
+                c => new
+                    {
+                        OrderDetailId = c.Int(nullable: false, identity: true),
+                        OrderId = c.Int(nullable: false),
+                        ProductId = c.Int(nullable: false),
+                        Quantity = c.Int(nullable: false),
+                        UnitPrice = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.OrderDetailId)
+                .ForeignKey("dbo.Order", t => t.OrderId, cascadeDelete: true)
+                .ForeignKey("dbo.Product", t => t.ProductId, cascadeDelete: true)
+                .Index(t => t.OrderId)
+                .Index(t => t.ProductId);
+            
+            CreateTable(
+                "dbo.Order",
+                c => new
+                    {
+                        OrderId = c.Int(nullable: false, identity: true),
+                        Username = c.String(),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        Address = c.String(),
+                        City = c.String(),
+                        State = c.String(),
+                        PostalCode = c.String(),
+                        Country = c.String(),
+                        Phone = c.String(),
+                        Email = c.String(),
+                        Total = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        OrderDate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.OrderId);
+            
             CreateTable(
                 "dbo.IdentityRole",
                 c => new
@@ -85,15 +150,25 @@ namespace FoodSpin.Data.Migrations
             DropForeignKey("dbo.IdentityUserLogin", "ApplicationUser_Id", "dbo.ApplicationUser");
             DropForeignKey("dbo.IdentityUserClaim", "ApplicationUser_Id", "dbo.ApplicationUser");
             DropForeignKey("dbo.IdentityUserRole", "IdentityRole_Id", "dbo.IdentityRole");
+            DropForeignKey("dbo.OrderDetail", "ProductId", "dbo.Product");
+            DropForeignKey("dbo.OrderDetail", "OrderId", "dbo.Order");
+            DropForeignKey("dbo.Cart", "ProductId", "dbo.Product");
             DropIndex("dbo.IdentityUserLogin", new[] { "ApplicationUser_Id" });
             DropIndex("dbo.IdentityUserClaim", new[] { "ApplicationUser_Id" });
             DropIndex("dbo.IdentityUserRole", new[] { "ApplicationUser_Id" });
             DropIndex("dbo.IdentityUserRole", new[] { "IdentityRole_Id" });
+            DropIndex("dbo.OrderDetail", new[] { "ProductId" });
+            DropIndex("dbo.OrderDetail", new[] { "OrderId" });
+            DropIndex("dbo.Cart", new[] { "ProductId" });
             DropTable("dbo.IdentityUserLogin");
             DropTable("dbo.IdentityUserClaim");
             DropTable("dbo.ApplicationUser");
             DropTable("dbo.IdentityUserRole");
             DropTable("dbo.IdentityRole");
+            DropTable("dbo.Order");
+            DropTable("dbo.OrderDetail");
+            DropTable("dbo.Product");
+            DropTable("dbo.Cart");
         }
     }
 }
